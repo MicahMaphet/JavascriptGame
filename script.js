cc525be7da44d6ef436e7968796f05aa12ed9f78
hotpig <93409262+MicahMaphet@users.noreply.github.com>
2022-06-09 19:57:07 +0000

    Working on the obstacles collision

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
 function move(x, y) {
+  var wallwidth = 
+    parseInt(window.getComputedStyle(wall)
+    .getPropertyValue("width"));
+
+  var wallheight = 
+    parseInt(window.getComputedStyle(wall)
+    .getPropertyValue("height"));
+
+  var wallleft = 
+    parseInt(window.getComputedStyle(wall)
+    .getPropertyValue("left"));
+  
+  var walltop = 
+    parseInt(window.getComputedStyle(wall)
+    .getPropertyValue("top"));
+  
   var left = 
     parseInt(window.getComputedStyle(square)
-             .getPropertyValue("left"));
-  if (left >= 20&&x === -20) {
-  square.style.left = left + x + "px";
-  }
-  if (left <= 260&&x === 20) {
-  square.style.left = left + x + "px";
-  }
-
+    .getPropertyValue("left"));
+  
   var top = 
     parseInt(window.getComputedStyle(square)
              .getPropertyValue("top"));
+  
+  if (left >= 20&&x === -20) {
+
+    if (top > wallheight + walltop
+       ||top < wallheight - walltop
+       ||left > wallleft + wallwidth
+       ||left < wallleft + wallwidth) 
+    {
+      square.style.left = left + x + "px";
+    } 
+  }
+  
+  if (left <= 260&&x === 20) {
+
+    if (top > wallheight + walltop
+       ||top < wallheight - walltop
+       ||left > wallleft - wallwidth
+       ||left < wallleft - wallwidth) 
+    {
+      square.style.left = left + x + "px";
+    }
+  }
+
+
   if (top >= 20&&y === -20) {
   square.style.top = top + y + "px";
   }
