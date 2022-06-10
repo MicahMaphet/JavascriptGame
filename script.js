748c0368384ff6c617d320e9c956d0e86a20969f
hotpig <93409262+MicahMaphet@users.noreply.github.com>
2022-06-10 15:22:26 +0000

    The maze part is finished

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var y =
 var yp = y; //this will keep track of the position for the collision
 
 function colide(a) {
-  var wallwidth = 
+  var wallwidth =
     parseInt(window.getComputedStyle(a)
     .getPropertyValue("width"));
 
@@ -44,19 +44,43 @@ function move(xc, yc) {
 
   x = x + xc;
   y = y + yc;
-  
+  /* the function colide makes sure that
+  if the guy went to the x and y quardanites
+  would he be in a wall or not. If it is in a wall
+  it goes back to the last position, if it is in 
+  a wall then it goes back to its last position*/
   if (colide(wall)
      ||colide(wall2)
      ||colide(wall3)
-     ||colide(wall4)) {
+     ||colide(wall4)
+     ||colide(wall5)
+     ||colide(wall6)
+     ||colide(wall7)
+     ||colide(wall8)
+     ||colide(wall9)
+     ||colide(wall10)
+     ||colide(wall11)
+     ||colide(wall12)
+     ||colide(wall13)
+     ||colide(wall14)
+     ||colide(wall15)
+     ||colide(wall16)
+     ||colide(wall17)
+     ||colide(wall18)
+     ||colide(wall19)
+     ||colide(wall20)
+     ||colide(wall21)
+     ||colide(wall22)
+     ||colide(wall23)
+     ||colide(wall24)
+     ||colide(wall25)) {
     x = xp;
     y = yp;
-  square.style.left = x + "px";
-  square.style.top = y + "px"; 
-  } else {
-  square.style.left = x + "px";
-  square.style.top = y + "px"; 
+  } else if (colide(win)) {
+    alert("you win");
   }
+  square.style.left = x + "px";
+  square.style.top = y + "px"; 
   
   //all the movement happens here
   xp = x;
